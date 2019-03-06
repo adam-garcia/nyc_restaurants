c105096e7a55776fe2d71ce0d373acea3312ba68
adam <aegarcia@bu.edu>
2019-03-05 22:57:03 -0500

    main setup, library loading

--- script/0-main.R
+++ script/0-main.R
@@ -1,2 +1,9 @@
 #   ____________________________________________________________________________
 #   0-main.R                                                                ####
+library(tidyverse)
+library(magrittr)
+library(lubridate)
+library(janitor)
+library(purrr)
+library(glue)
+library(maptools)
