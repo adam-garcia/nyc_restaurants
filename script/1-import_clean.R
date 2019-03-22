5308b6b514e56e6153736d0895035a72fd014248
adam <aegarcia@bu.edu>
2019-03-22 19:32:09 -0400

    programmatic mapquest scrape

--- script/1-import_clean.R
+++ script/1-import_clean.R
@@ -13,9 +13,18 @@ library(purrr)
 library(glue)
 library(maptools)
 library(viridis)
-library(RSelenium)
+library(httr)
+library(jsonlite)
 library(rvest)
 library(broom)
+library(rgdal)
+library(feather)
+
+# ggplot2 theme
+theme_set(theme_minimal())
+
+# metadata environment
+meta <- new.env()
 
 ##  ............................................................................
 ##  Restaurant data from nyc domh                                           ####
@@ -47,7 +56,7 @@ nyc_in <- read_csv("data/DOHMH_New_York_City_Restaurant_Inspection_Results.csv")
   # From the data dictionary:
   # inspection_dates of 1/1/1900 have not yet been inspected
   filter(inspection_date != mdy("1/1/1900")) %>% 
-  # Drop 'Missing' Boroughs
+  # Drop 'Missing' Boroughs (there's only 7 of the > 25,000)
   filter(boro != "Missing")
 
 # Some columns (grade) don't hold fidelity within inspection dates
@@ -60,6 +69,9 @@ nyc <- nyc_in %>%
   select(-grade, -grade_date) %>%
   left_join(fix_na_cols) %>%
   mutate(
+    street = str_replace_all(street, "\\s{2,}", " "),
+    place = str_c(building, street, sep = " "),
+    address = str_c(place, boro, "NY", zipcode, sep = ", "),
     grade = case_when(
       str_detect(action, "(C|c)losed") ~ "X",
       T ~ grade
@@ -80,14 +92,13 @@ nyc <- nyc_in %>%
   group_by(camis) %>% 
   arrange(desc(inspection_date)) %>% 
   slice(1) %>% 
-  ungroup() 
+  ungroup()
 
 ##  ............................................................................
 ##  City of New York borough spatial data                                   ####
 
-# https://data.cityofnewyork.us/City-Government/Borough-Boundaries/tqmj-j8zm
 # Read Geospatial data of boroughs
-boro_sp <- rgdal::readOGR("sandbox/geo_export_bf24ed11-bed6-49a3-8af5-009cdbec9260.shp")
+boro_sp <- readOGR("sandbox/geo_export_bf24ed11-bed6-49a3-8af5-009cdbec9260.shp")
 
 # Extract rownames
 boro_sp@data$id <- rownames(boro_sp@data)
@@ -110,8 +121,6 @@ boro_map <- fortify(nyc_sp) %>%
 
 ##  ............................................................................
 ##  Pediacities neighborhood spatial data                                   ####
-
-# http://data.beta.nyc/dataset/pediacities-nyc-neighborhoods/resource/35dd04fb-81b3-479b-a074-a27a37888ce7
 
 # Read GeoJSON data into Spatial Polygon data frame
 nbhd_geojson <- readOGR(
@@ -136,14 +145,118 @@ nbhd_map <- nbhd_geojson %>%
   left_join(nbhd_dict) %>% 
   mutate(group = as.numeric(group) %>% floor(),
          nbhd_boro = borough %>% 
-           fct_relevel(c("Manhattan", "Bronx", "Brooklyn", "Queens")))
+           fct_relevel(c("Manhattan", "Bronx", "Brooklyn", "Queens"))) %>% 
+  rename(nbhd = neighborhood)
+
+##  ............................................................................
+##  MapQuest Geocoding                                                      ####
+
+# MapQuest limits to 15,000 requests per key
+# Manhattan and the Bronx have 12,585 entries, so we'll split there
+mq_split <- nyc %>% 
+  mutate(manbx = boro %in% c("Manhattan", "Bronx")) %>% 
+  split(.$manbx) %>% 
+  # clean_names()
+  set_names(c("bknqs", "manbx"))
+
+# Load api keys from private directory
+source("private/api_key.R")
+
+# Function get_mq_loc
+## Mapquest location scrape function
+# Arguments
+## loc: Address or address-like location to geocode
+## id: identifier value for relational merging
+## boro: which borough group api key to use
+get_mq_loc <- function(loc, id, boro) {
+  # Return NA lat/lng for missing addresses
+  if(is.na(loc)) {
+    tibble(camis = id, lat = NA, lng = NA, status = "NA Address") %>% 
+      return()
+  }
+  # URL Template
+  mq_url <-glue("http://www.mapquestapi.com/geocoding/v1/address?key=",
+                "{get_mq_key(boro)}&location={loc}&maxResults=1") %>% 
+      URLencode()
+  # Perform the request
+  request <- mq_url %>% 
+    httr::GET() %>% 
+    content("text") %>% 
+    fromJSON()
+  # Only parse the data if the request was successful
+  if(request$info$statuscode == 0) {
+    # Pull lat/lng field
+    request %>% 
+      .[["results"]] %>% 
+      .[["locations"]] %>% 
+      first() %>% 
+      .[["latLng"]] %>% 
+      as_tibble() %>% 
+      mutate(camis = id, status = "Success") %>% 
+      rename(long = lng) %>% 
+      return()
+  } else {
+    tibble(
+      camis = id, 
+      long = NA, lat = NA, 
+      stauts = request$info$statuscode
+    ) %>% 
+      return()
+  }
+}
 
 
+### . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ..
+### Map over locations by borough                                           ####
+# Only run if it hasn't been run before
+if(!file.exists("data/nyc_locs.csv")) {
+  # Manhattan and the Bronx
+  manbx_start <- Sys.time()
+  manbx_locs <- list(
+    mq_split$manbx$address,
+    mq_split$manbx$camis,
+    "manbx"
+  ) %>% 
+    pmap_df(get_mq_loc)
+  manbx_time <- Sys.time() - manbx_start
+  
+  # Brooklyn, Queens, Staten Island
+  bknqs_start <- Sys.time()
+  bknqs_locs <- list(
+    mq_split$bknqs$address,
+    mq_split$bknqs$camis,
+    "bknqs"
+  ) %>% 
+    pmap_df(get_mq_loc)
+  
+  # Some of the geocoding wasn't accurate
+  # Filter to bounding box of our map data to double check
+  bbox <- nbhd_geojson@bbox %>% 
+    as_tibble()
+  
+  # Attach locations to nyc data
+  nyc_locs_in <- bind_rows(manbx_locs, bknqs_locs) %>% 
+    filter(between(long, bbox$min[1], bbox$max[1]),
+           between(lat, bbox$min[2], bbox$max[2]))
+  
+  nyc_locs <- left_join(nyc, nyc_locs_in)
+  
+  # Write geocoded locations to disk
+  write_csv(nyc_locs, "data/nyc_locs.csv")
+} else {
+  nyc_locs <- read_csv("data/nyc_locs.csv")
+}
 
 ##  ............................................................................
 ##  Neighborhood coding using sp::over                                      ####
+nyc_sp <- nyc_locs %>% 
+  filter(!is.na(lat))
+coordinates(nyc_sp) <- ~ long + lat
+proj4string(nyc_sp) <- proj4string(nbhd_geojson)
 
-
+nyc_map <- nyc_locs %>% 
+  filter(!is.na(lat)) %>% 
+  mutate(nbhd = over(nyc_sp, nbhd_geojson)$neighborhood)
 
 ##  ............................................................................
 ##  Data inspection                                                         ####
@@ -151,12 +264,8 @@ nbhd_map <- nbhd_geojson %>%
 
 # First, let's explore our dataset
 
-# From the documentation, "Missing" is a boro category.
-# We'll drop those, there's only 7 of the > 25,000
-
-nyc <- filter(nyc, boro != "Missing")
-
 nyc_skim <- skimr::skim(nyc) 
+
 print(nyc_skim)
 
 # Next, let's get into missing data
@@ -217,8 +326,5 @@ nyc %>%
 ##  Cuisine is pretty messy, let’s look closer                              ####
  
 nyc %>% 
-  count(cuisine) %>% 
-  ggplot(aes(x = reorder(cuisine, n), y = n)) +
-  geom_bar(stat = "identity") +
-  coord_flip()
+  count(cuisine, sort = T)
 
